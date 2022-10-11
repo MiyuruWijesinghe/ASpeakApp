ef297af962f77bbc4623a46faebdd65f4326fdc2
Miyuru Wijesinghe <MiyuruW@lolctech.com>
2022-10-11 15:21:55 +0530

    updated

---
+++ android/app/src/main/java/com/tenserflow/aspeakapp/SplashScreen.kt
@@ -0,0 +1,20 @@
+package com.tenserflow.aspeakapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreen : AppCompatActivity() {
+    private val SPLASH_TIME : Long = 10000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, SPLASH_TIME)
+    }
+}
